adc7e27899c9fb0bd19bd25a5db75a987e035226
fispurring <llxxyy55@live.cn>
2025-10-29 22:34:19 +0800

    version: 0.4.0

--- info.py
+++ info.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 # 项目信息
 APP_NAME = "LightVT"
-APP_VERSION = "0.3.1"
-APP_DESCRIPTION = "视频字幕提取与翻译工具"
+APP_VERSION = "0.4.0"
+APP_DESCRIPTION = "Video Subtitle Extraction and Translation Tool"
 APP_AUTHOR = "Marsyas"
